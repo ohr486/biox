336fd152542396cedfee68f09ed2240ae5b18b60
ohr486 <ohr486@gmail.com>
2017-09-19 12:19:48 +0900

    update dir struct

--- lib/buby/mix.exs
+++ lib/buby/mix.exs
@@ -4,7 +4,7 @@ defmodule Buby.Mixfile do
   def project do
     [
       app: :buby,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps()
