28cac9471ec945565c1b427ce9218762891b7ba2
ohr486 <ohr486@gmail.com>
2018-06-02 19:11:01 +0900

    1.5 -> 1.6

--- lib/buby/mix.exs
+++ lib/buby/mix.exs
@@ -1,12 +1,12 @@
-defmodule Buby.Mixfile do
+defmodule Buby.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :buby,
-      version: "0.0.1",
-      elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
+      version: "0.1.0",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
   end
